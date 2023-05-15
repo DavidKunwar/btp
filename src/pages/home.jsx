ce0255c18af4f35b429526994cef40a3b3b58dc9
David Kunwar <dkunwar7111@gmail.com>
2023-05-15 13:45:50 +0530

    Added homepage content

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -42,6 +42,27 @@ function Home() {
                     <Profile profile={profile} setProfile={setProfile} />
                 ) : (
                     <>
+                        <div>
+                            Activity Tracker
+                        </div>
+                        <p>
+                            Welcome to our application, where we offer a unique service that tracks your screen time and provides an analysis of whether you may be at risk of developing Attention Deficit Hyperactivity Disorder (ADHD).
+                        </p>
+                        <p>
+                            We understand that in today's digital age, screens have become an integral part of our daily lives. While they provide us with convenience and entertainment, excessive screen time can lead to several negative effects, including a decrease in attention span and focus. These effects can be particularly problematic for individuals who are at risk of developing ADHD.
+                        </p>
+                        <p>
+                            Our website aims to provide you with a comprehensive analysis of your screen time habits and how they may be impacting your overall well-being. By tracking your usage patterns across different devices, we can provide insights into the amount of time you spend on specific apps or websites and how this may be affecting your concentration levels.
+                        </p>
+                        <p>
+                            Through our analysis, we provide personalized recommendations on how to reduce screen time and improve attention span. Our service also includes resources for individuals who may already be struggling with ADHD, such as support groups and professional counseling services.
+                        </p>
+                        <p>
+                            At our website, we prioritize the privacy and security of our users, ensuring that all data is kept confidential and secure. We understand that this is a sensitive issue, and we aim to provide a safe and supportive space for our users.
+                        </p>
+                        <p>
+                            Thank you for considering our service. We hope that our analysis will provide you with valuable insights into your screen time habits and how you can improve your overall well-being.
+                        </p>
                         <Button variant="contained" onClick={() => login()} startIcon={<GoogleIcon />}>
                             Sign in with Google 🚀
                         </Button>
